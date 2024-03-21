cc1cc551d75b89185a48baf275bf8181d8a836cb
David Sover <alexsover@gmail.com>
2024-03-21 10:53:52 -0400

    add src files

--- src/non-compliant.sql
+++ src/non-compliant.sql
@@ -1,19 +1,19 @@
 -- Long line, mixed case
 SELECT
-    FIRST_NAME
-    , LAST_NAME
-    , EMAIL
-    , TEST_COLUMN
-    , TEST_COLUMN_2
-FROM USERS WHERE STATUS = 'active' AND EMAIL LIKE '%@example.com';
+    first_name
+    , last_name
+    , email
+    , test_column
+    , test_column_2
+FROM users WHERE status = 'active' AND email LIKE '%@example.com';
 
 -- Single line, mixed case, aliases not lowercase
 SELECT
-    U.FIRST_NAME
-    , U.LAST_NAME
-    , O.ORDER_DATE
-    , O.ORDER_AMOUNT
-FROM USERS AS U
-INNER JOIN ORDERS AS O ON U.USER_ID = O.USER_ID
-WHERE U.STATUS = 'active'
-ORDER BY O.ORDER_DATE DESC;
+    u.first_name
+    , u.last_name
+    , o.order_date
+    , o.order_amount
+FROM users AS u
+INNER JOIN orders AS o ON u.user_id = o.user_id
+WHERE u.status = 'active'
+ORDER BY o.order_date DESC;
--- src/sql_a_feat.sql
+++ src/sql_a_feat.sql
@@ -1,3 +1,8 @@
-select col1
-from
-    table2
+create temporary table june23 (
+    ip_address_id VARCHAR(16777216)
+    , ramp_id VARCHAR(16777216)
+);
+create table june23 (
+    ip_address_id VARCHAR(16777216)
+    , ramp_id VARCHAR(16777216)
+);
